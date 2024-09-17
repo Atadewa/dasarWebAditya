ebce24c193ea874ee9b8c0252f5d5c405408eb8c
Atadewa <aditafli42@gmail.com>
2024-09-17 08:41:16 +0700

    menambahkan operator pembanding pada php

--- Praktikum-4/operator.php
+++ Praktikum-4/operator.php
@@ -15,4 +15,19 @@ echo "Hasil dari $a dikali $b adalah $hasilKali <br>";
 echo "Hasil dari $a dibagi $b adalah $hasilBagi <br>";
 echo "Hasil dari $a sisa bagi $b adalah $sisaBagi <br>";
 echo "Hasil dari $a pangkat $b adalah $pangkat <br>";
+
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+echo "<br>";
+echo "Hasil perbandingan $a sama dengan $b : " . ($hasilSama ? 'true' : 'false') . "<br>";
+echo "Hasil perbandingan $a tidak sama dengan $b : " . ($hasilTidakSama ? 'true' : 'false') . "<br>";
+echo "Hasil perbandingan $a lebih kecil dari $b : " . ($hasilLebihKecil ? 'true' : 'false') . "<br>";
+echo "Hasil perbandingan $a lebih besar dari $b : " . ($hasilLebihBesar ? 'true' : 'false') . "<br>";
+echo "Hasil perbandingan $a lebih kecil dari atau sama dengan $b : " . ($hasilLebihKecilSama ? 'true' : 'false') . "<br>";
+echo "Hasil perbandingan $a lebih besar dari atau sama dengan $b : " . ($hasilLebihBesarSama ? 'true' : 'false') . "<br>";
 ?>
