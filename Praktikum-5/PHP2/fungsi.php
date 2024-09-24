a3089c9acdefea09c1de6c15dd52cd3758a992b7
Atadewa <aditafli42@gmail.com>
2024-09-24 09:53:49 +0700

    membuat fungsi dengan nilai return

--- Praktikum-5/PHP2/fungsi.php
+++ Praktikum-5/PHP2/fungsi.php
@@ -1,5 +1,15 @@
 <?php 
 
+// Membuat fungsi
+function hitungUmur($thn_lahir, $thn_sekarang) {
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+
+echo "Umur saya adalah " . hitungUmur(2005, 2024) . " tahun";
+
+echo "<br/><br/>";
+
 function perkenalan($nama, $salam="Assalamualaikum"){
     echo $salam.", ";
     echo "Perkenalkan, nama saya " .$nama. "<br/>";
