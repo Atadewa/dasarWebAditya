20d3046cf07cd0dcf24b660501006c91e9980038
Atadewa <aditafli42@gmail.com>
2024-09-24 09:33:41 +0700

    membuat fungsi pada php

---
+++ Praktikum-5/PHP2/fungsi.php
@@ -0,0 +1,12 @@
+<?php 
+
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Aditya Atadewa<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+// Memanggil fungsi yang sudah dibuat
+perkenalan();
+
+?>
