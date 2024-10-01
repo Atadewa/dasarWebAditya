4dd27133e59a6df069f3e3e0df6c1daec88cb5d7
Atadewa <aditafli42@gmail.com>
2024-10-01 08:38:40 +0700

    membuat function dalam php

---
+++ Praktikum-6/praktik_php/fungsi.php
@@ -0,0 +1,21 @@
+<?php 
+
+// Membuat fungsi
+function hitungUmur($thn_lahir, $thn_sekarang) {
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+
+function perkenalan($nama, $salam="Assalamualaikum"){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya " .$nama. "<br/>";
+
+    // Memanggil fungsi lain
+    echo "Saya berusia " . hitungUmur(2005, 2024) . " tahun<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+// Memanggil fungsi perkenalan
+perkenalan("Aditya Atadewa");
+
+?>
