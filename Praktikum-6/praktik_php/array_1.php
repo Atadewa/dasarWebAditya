c2482ff9a3eb031caee849d6982962f2d36ceabd
Atadewa <aditafli42@gmail.com>
2024-10-01 07:19:35 +0700

    membuat array dalam php

---
+++ Praktikum-6/praktik_php/array_1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Array</title>
+</head>
+<body>
+    <h2>Array Terindeks</h2>
+
+    <?php 
+        $Listdosen = ["Elok Nur Hamdana", "Unggul Pemenang", "Bagus Nugraha"];
+
+        echo $Listdosen[2] . "<br>";
+        echo $Listdosen[0] . "<br>";
+        echo $Listdosen[1] . "<br>";
+    ?>
+</body>
+</html>
