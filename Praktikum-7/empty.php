4bc130545dff694dbecb1d061c283663e6644438
Atadewa <aditafli42@gmail.com>
2024-10-08 07:36:51 +0700

    menambahkan fungsi empty untuk variabel yang belum dideklarasikan

--- Praktikum-7/empty.php
+++ Praktikum-7/empty.php
@@ -7,4 +7,11 @@ if (empty($myArray)) {
     echo "Array terdefinisi dan tidak kosong.";
 }
 
+echo "<br>";
+if (empty($nonExistentVar)) {
+    echo "Array tidak terdefinisi atau kosong.";
+} else {
+    echo "Array terdefinisi dan tidak kosong.";
+}
+
 ?>
