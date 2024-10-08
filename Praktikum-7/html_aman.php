0012c4dcb288e19e82d8e71ba1ae12c619be599c
Atadewa <aditafli42@gmail.com>
2024-10-08 11:23:59 +0700

    menambahkan form email dan proses validasi email

--- Praktikum-7/html_aman.php
+++ Praktikum-7/html_aman.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Form Input Aman</title>
+    <title>Form Input</title>
 </head>
 <body>
 
@@ -10,16 +10,27 @@
     <label for="input">Masukkan Input:</label>
     <input type="text" id="input" name="input" required><br><br>
 
+    <label for="email">Masukkan Email:</label>
+    <input type="email" id="email" name="email" required><br><br>
+
     <input type="submit" value="Kirim">
 </form>
 <br>
 
 <?php
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $input = $_POST['input'];
+    $email = $_POST['email'];
+
     $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
 
-    echo "Input yang aman: " . $input . "<br>";
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Input yang aman: " . $input . "<br>";
+        echo "Email yang valid: " . $email;
+    } else {
+        echo "Email tidak valid. Silakan coba lagi.";
+    }
 }
 ?>
 
